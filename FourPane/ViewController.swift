d773481a6735acb25b90e301a9f5366185643f76
Angelina <kebeke@list.ru>
2022-10-27 12:46:58 +0300

    Commit

--- FourPane/ViewController.swift
+++ FourPane/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import MapKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let url = URL(string: "https://www.tripadvisor.ru/Hotel_Review-g297967-d6451441-Reviews-Throne_Beach_Resort_Spa-Manavgat_Turkish_Mediterranean_Coast.html")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
         // Do any additional setup after loading the view.
     }
 
-
+    
+    
 }
 
