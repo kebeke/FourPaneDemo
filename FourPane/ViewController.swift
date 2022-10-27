f919cc654038c1b5192f36bcbd7381c491f46963
Angelina <kebeke@list.ru>
2022-10-27 13:27:55 +0300

    Commit

--- FourPane/ViewController.swift
+++ FourPane/ViewController.swift
@@ -21,13 +21,21 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = URL(string: "https://www.tripadvisor.ru/Hotel_Review-g297967-d6451441-Reviews-Throne_Beach_Resort_Spa-Manavgat_Turkish_Mediterranean_Coast.html")!
+        let url = URL(string: "https://www.thronebeachresort.com/")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
         // Do any additional setup after loading the view.
+       let initialLocation = CLLocation(latitude: 36.673653496769205, longitude: 31.639222883902683)
+        mapView.centerLocation(initialLocation)
     }
 
     
     
 }
 
+extension MKMapView {
+    func centerLocation(_ location: CLLocation, rerionRadius: CLLocationDistance = 1000) {
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: rerionRadius, longitudinalMeters: rerionRadius)
+        setRegion(coordinateRegion, animated: true)
+    }
+}
